0575ee6a976a280966cd954a2a07ac257c8b5239
rafjas2 <rafal_jasinski@yahoo.ie>
2019-10-13 23:35:32 +0100

    add bgcolor

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8050;
 app.use(express.static(__dirname + "/public"));
 app.get("/", function(req, res){
     res.render("index");
